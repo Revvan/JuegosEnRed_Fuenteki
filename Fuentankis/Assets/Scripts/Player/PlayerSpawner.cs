22b4d6bfde8f7cd1e4149d516090f943e86946c2
MateSalado <181142278+MateSalado@users.noreply.github.com>
2026-09-07 19:07:03 -0300

    Revert "Merge branch 'Esteban-DV' into Mate_DV"
    This reverts commit a02042b2f1e558429e502abd765ed1ef203348c0, reversing
    changes made to 42d51f27ca68a5b381ead15f8549afaecc5dff99.

--- Fuentankis/Assets/Scripts/Player/PlayerSpawner.cs
+++ Fuentankis/Assets/Scripts/Player/PlayerSpawner.cs
@@ -1,41 +1,20 @@
 using UnityEngine;
 using Photon.Pun;
-using System.Collections.Generic;
 
-public class PlayerSpawner : MonoBehaviourPun
+public class PlayerSpawner : MonoBehaviour
 {
 
     [SerializeField] private GameObject playerPrefab;
-
-    [SerializeField] private List<Transform> spawnPoint;
-    public int playerCount = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        PhotonManager.Instance.OnRoom += PlayerJoinedRoom;
-        if(spawnPoint.Count <=0){
-            Debug.Log("no spawn point");
-        }
+        PhotonManager.Instance.OnRoom += SpawnPlayer;
     }
 
 
     private void SpawnPlayer()
     {
-        PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint[playerCount].position, Quaternion.identity);
-        playerCount++;
-        if(playerCount > spawnPoint.Count){
-            Debug.Log("spawn point overflow");
-            playerCount = 0;
-        }
-    }
-
-    [PunRPC]
-    public void RPC_SpawnPlayer(){
-        SpawnPlayer();
-    }
-
-    private void PlayerJoinedRoom(){
-        photonView.RPC("RPC_SpawnPlayer", RpcTarget.AllBuffered);
+        PhotonNetwork.Instantiate(playerPrefab.name, transform.position, Quaternion.identity);
     }
 
 }
