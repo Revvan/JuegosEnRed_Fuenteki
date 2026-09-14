fdd14039c42dafb058e4db777ed334eb03bb823a
CrazyLizard2020 <69931632+CrazyLizard2020@users.noreply.github.com>
2026-09-13 23:47:43 -0300

    [FIX?] Inverted isMasterClient condition
    Maybe?

--- Fuentankis/Assets/Scripts/Environment/LevelGenerator.cs
+++ Fuentankis/Assets/Scripts/Environment/LevelGenerator.cs
@@ -20,28 +20,26 @@ public class LevelGenerator : MonoBehaviourPunCallbacks
 
     private IEnumerator SetUpLevel(int seed)
     {
-        if (!PhotonNetwork.IsMasterClient)
+        if (PhotonNetwork.IsMasterClient)
         {
+            seed = Random.Range(10000, 99999);
+            Random.InitState(seed);
+
+            int obstaclesAmount = Random.Range(obstaclesMinAmount, obstaclesMaxAmount);
+
+            for (int i = 0; i < obstaclesAmount; i++)
+            {
+                int x = (int)(Random.value * (worldSpace.x));
+                int y = (int)(Random.value * (worldSpace.y));
+
+                GameObject go = PhotonNetwork.InstantiateRoomObject(wallPrefab.name, new Vector3(x + origin.x, y + origin.y, worldSpace.z), Quaternion.identity);
+                go.transform.localScale = new Vector3(Random.value + 1 + go.transform.localScale.x, Random.value + 0.8f + go.transform.localScale.y, go.transform.localScale.z);
+                go.transform.rotation *= Quaternion.Euler(0, 0, (Random.Range(-100, 100)));
+                //Debug.Log(go.transform.localScale);
+                go.transform.SetParent(gameObject.transform);
+            }
+
             yield return new WaitForEndOfFrame();
         }
-
-        seed = Random.Range(10000, 99999);
-        Random.InitState(seed);
-
-        int obstaclesAmount = Random.Range(obstaclesMinAmount, obstaclesMaxAmount);
-
-        for (int i = 0; i < obstaclesAmount; i++)
-        {
-            int x = (int)(Random.value * (worldSpace.x));
-            int y = (int)(Random.value * (worldSpace.y));
-
-            GameObject go = PhotonNetwork.InstantiateRoomObject(wallPrefab.name, new Vector3(x + origin.x, y + origin.y, worldSpace.z), Quaternion.identity);
-            go.transform.localScale = new Vector3(Random.value + 1 + go.transform.localScale.x, Random.value + 0.8f + go.transform.localScale.y, go.transform.localScale.z);
-            go.transform.rotation *= Quaternion.Euler(0, 0, (Random.Range(-100, 100)));
-            //Debug.Log(go.transform.localScale);
-            go.transform.SetParent(gameObject.transform);
-        }
-
-        yield return new WaitForEndOfFrame();
     }
 }
